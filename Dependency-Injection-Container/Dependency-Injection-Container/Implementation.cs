daf3859b27a848d2d08fa4dde9b1e83acc67ff9f
Sergei <s.bysov@mail.ru>
2018-12-15 17:38:20 +0300

    Add Implementation class

---
+++ Dependency-Injection-Container/Dependency-Injection-Container/Implementation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Injection_Container
+{
+    public class Implementation
+    {
+        public Type Type { get; private set; }
+        public bool IsSingleton { get; private set; }
+
+        public Implementation(Type type, bool isSingleton)
+        {
+            Type = type;
+            IsSingleton = isSingleton;
+        }
+    }
+}
